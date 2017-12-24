d89c6ec4090c0a9c2f93b30b319d3b407ec1d1f0
Mark R <mirubenstein@gmail.com>
2017-12-23 22:49:17 -0800

    Recursive pass

--- sieve.rb
+++ sieve.rb
@@ -1,14 +1,22 @@
-require 'prime'
-
 class Sieve
   attr_reader :number
 
   def initialize(number)
     @number = number
+    @primes = []
   end
 
   def primes
-    Prime.entries number
+    recursive_primes((2..@number).to_a)
+  end
+
+  def recursive_primes(numbers)
+    if numbers.empty?
+      @primes
+    else
+      @primes << numbers.shift
+      recursive_primes(numbers.reject { |num| (num % @primes.last).zero? })
+    end
   end
 end
 
